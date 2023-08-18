6f2a90ec5a9e7ceccfa4dfb4be7c3194f06424c3
estebandev <brayan.villa.fer@gmail.com>
2023-08-17 21:57:18 -0500

    add fcv logo and change it to white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,18 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import pathImages from '@/cons/pathImages'
 
-export default function Home() {
+export default function Home () {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <section className={styles.contentMain}>
+      <section className={styles.contentImage}>
+        <Image src={pathImages.bgImage1} alt='' className={styles.image} width={1920} height={1080} />
+        <div className={styles.filterBlack} />
+        <Image src={pathImages.logoFcv} alt='' className={styles.logo} width={300} height={300} />
+      </section>
+      <main className={styles.main}>
+        <h1 className={styles.title}>HELPDESK</h1>
+      </main>
+    </section>
   )
 }
---
+++ src/cons/pathImages.ts
@@ -0,0 +1,17 @@
+const pathBgImage1 = '/imagen-helpdesk1.jpg'
+const pathBgImage2 = '/imagen-helpdesk2.avif'
+const pathLogo = '/LOGO-FCV.png'
+
+type PathImages = {
+  bgImage1: string
+  bgImage2: string
+  logoFcv: string
+}
+
+const pathImages: PathImages = {
+  bgImage1: pathBgImage1,
+  bgImage2: pathBgImage2,
+  logoFcv: pathLogo
+}
+
+export default pathImages
